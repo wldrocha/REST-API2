10e65865cc62f20a05a4a7ae8f1f30fc27a7f61a
Wladimir Rocha <wld.rocha@gmail.com>
2019-12-03 10:39:50 -0400

    mejoras varias

--- src/index.js
+++ src/index.js
@@ -2,30 +2,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const {
 	appName,
 	port,
-	database: { name, url }
+	database: { name, url },
 } = require('./app.config');
 const { connect } = require('mongoose');
-
 
 const app = express();
 app.use(cors());
 app.use(
 	express.json({
-		limit: '20mb'
+		limit: '20mb',
 	})
 );
 app.use(
 	express.urlencoded({
-		extended: false
+		extended: false,
 	})
 );
-app.use('/public', express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(compression());
 app.use(morgan('tiny'));
 app.use(helmet());
@@ -34,44 +32,26 @@ app.use(helmet());
 const userRoutes = require('./routes/user.routes');
 const productRoutes = require('./routes/product.routes');
 const supplierRoutes = require('./routes/supplier.routes');
+const paymentRoutes = require('./routes/payment.routes');
 // import userRoutes from './routes/user.routes';
 
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(supplierRoutes);
+app.use(paymentRoutes);
 
-// app.use(function (req, res, next) {
-
-// 	// Website you wish to allow to connect
-// 	res.setHeader('Access-Control-Allow-Origin', '*');
-
-// 	// // Request methods you wish to allow
-// 	res.setHeader('Access-Control-Allow-Methods', '*');
-
-// 	// // Request headers you wish to allow
-// 	res.setHeader('Access-Control-Allow-Headers', 'content-type, authentication, *');
-
-// 	// // Set to true if you need the website to include cookies in the requests sent
-// 	// // to the API (e.g. in case you use sessions)
-// 	res.setHeader('Access-Control-Allow-Credentials', true);
-
-// 	// Pass to next layer of middleware
-// 	next();
-// });
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 async function main() {
 	await app.listen(port);
 	try {
-		await connect(
-			`${url}${name}`,
-			{
-				useNewUrlParser: true
-			}
-		);
+		await connect(`${url}${name}`, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
 		await console.log(`${appName} RESTful API server started on: ${port}`);
-	} catch (e) {
-		console.log(`${appName} RESTful API server it's off :( `, e);
+	} catch (error) {
+		console.log(`${appName} RESTful API server it's off :( `, error);
 	}
 }
 
--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,15 +1,15 @@
 const { verify } = require('jsonwebtoken');
 const {
-	tokens: { secretKey }
+	tokens: { secretKey },
 } = require('../app.config');
 
 module.exports = (req, res, next) => {
 	try {
-		const decoded = verify(req.headers.authentication.split(' ')[1], secretKey);
+		const decoded = await verify(req.headers.authorization.split(' ')[1], secretKey);
 		req.userDate = decoded;
 	} catch (error) {
 		return res.status(401).json({
-			message: 'Auth Failed.'
+			message: 'Auth Failed.',
 		});
 	}
 	next();
